fbff974a54fb36f1f34562f464c928e9eed4aaaa
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-08-15 06:46:30 -0500

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
+]
+
+module.exports = {
+  entry: "./app/assets/js/App.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  mode: "development",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader?url=false",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: postCSSPlugins,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
