b5363825bcffc535a36fc63a0b18c8ff8e76a4d9
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-08-15 16:31:01 -0500

    Add mixins for different screen size

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const postCSSPlugins = [
   require('postcss-import'),
+  require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
   require('autoprefixer')
@@ -17,7 +18,6 @@ module.exports = {
     hot: true,
     port: 3000,
     host: '0.0.0.0'
-
   },
   mode: "development",
   module: {
