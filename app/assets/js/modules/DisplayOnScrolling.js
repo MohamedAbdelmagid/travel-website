6725aff504b725a732e0a043b5e80a834c0bade1
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-08-23 16:40:14 -0500

    Make code more efficient

--- app/assets/js/modules/DisplayOnScrolling.js
+++ app/assets/js/modules/DisplayOnScrolling.js
@@ -1,27 +1,45 @@
+import throttle from 'lodash/throttle'
+
 class DisplayOnScrolling {
   constructor() {
     this.itemsToDisplay = document.querySelectorAll(".feature-item")
     this.hideItems()
+    this.scrollThrottle = throttle(this.displayCaller, 200).bind(this)
     this.events()
   }
 
   events() {
-    window.addEventListener("scroll", () => {
-      this.itemsToDisplay.forEach(item => {
+    window.addEventListener("scroll", this.scrollThrottle)
+  }
+
+  displayCaller() {
+    this.itemsToDisplay.forEach(item => {
+      if (item.isRevealed == false) {
         this.displayIfScrolledTo(item)
-      })
+      }
     })
   }
 
   displayIfScrolledTo(item) {
-    let apperancePercent = (item.getBoundingClientRect().top / window.innerHeight) * 100
-    if (apperancePercent < 75) {
-      item.classList.add("display-item--visible")
+    if (window.scrollY + window.innerHeight > item.offsetTop) {
+      let apperancePercent = (item.getBoundingClientRect().top / window.innerHeight) * 100
+      if (apperancePercent < 75) {
+        console.log("Item was displayed!!");
+        item.classList.add("display-item--visible")
+        item.isRevealed = true
+        if (item.isLastItem) {
+          window.removeEventListener("scroll", this.scrollThrottle)
+        }
+      }
     }
   }
 
   hideItems() {
-    this.itemsToDisplay.forEach(item => (item.classList.add("display-item")))
+    this.itemsToDisplay.forEach(item => {
+      item.classList.add("display-item")
+      item.isRevealed = false
+    })
+    this.itemsToDisplay[this.itemsToDisplay.length - 1].isLastItem = true
   }
 }
 
