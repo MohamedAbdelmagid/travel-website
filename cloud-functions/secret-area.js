1b52a1a57bc790aeedf82a54b3e28e24067aacc2
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-08-27 07:00:20 -0500

    Cloud function check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the secret area!!"
-  })
+  const content = `
+  <h3>This is the secret area!</h3>
+  <p>Be bold and don't give up <strong>Thanks god</strong>
+  `
+
+  let body
+  if (event.body) {
+    body = JSON.parse(event.body)
+  } else {
+    body = {}
+  }
+
+  if (body.password == "password") {
+    callback(null, {
+      statusCode: 200,
+      body: content
+    })
+    
+  } else {
+    callback(null, {
+      statusCode: 401,
+      body: "<h2 style='color: red'>Wrong password!!</h2>"
+    })
+  }
 }
